b77665c13cd0cf88ba033334dadd77e96e4d7ac6
saragoza <ssantander3@yahoo.es>
2017-06-03 18:40:10 +0200

    clickclass v1

--- resources/views/layouts/help.blade.php
+++ resources/views/layouts/help.blade.php
@@ -59,7 +59,7 @@
            </div>
            <h1>1  ¿Qué tengo que hacer para formar parte de la comunidad ClickClass?</h1>
            <p>Formar parte de la red de usuarios de ClickClass es muy sencillo, simplemente tienes que acceder al formulario de registro haciendo click en el link Registrarse de la welcome page.</p>
-           <p>Simplemente introduce tu nombre de usuario, email y password y con un click serás un miembro más de la comunidad.</p>
+           <p>Introduce tu nombre de usuario, email y password y con un click serás un miembro más de la comunidad.</p>
            <h1>2  ¿Cómo me logueo en la aplicación?</h1>
            <p>Haciendo click en en link Conectarse de la welcome page accedes a un formulario en el que debes introducir tu email de usuario y password.</p>
            <p>Si así lo deseas, puedes marcar la opción Recordarme, y tu password aparecerá automáticamente en el campo del formulario la próxima vez que te quieras conectar.</p>
@@ -69,22 +69,22 @@
            <h1>4  ¿Cómo puedo compartir archivos?</h1>
            <p>Seleccionando la opción Compartir accedes a un formulario en el que se te pedirán una serie de datos:</p>
            <ol>
-               <li>Descripción: Consiste en una breve definición del archivo. Procura que sea lo más descriptiva y concreta posible para que el resto de usuarios sepan qué van a encontrarse.</li>
-               <li>Tags de búsqueda: De forma opcional, puedes introducir tags que faciliten la búsqueda del archivo. Estos tags no saldrán en la información detallada ni en los listados.</li>
-               <li>Tipo de archivo: Selecciona el tipo de archivo que mejor encaje con el recurso que vas a compartir.</li>
-               <li>Ruta del archivo: Selecciona la ruta donde se encuentra el archivo a compartir.</li>
-               <li>Información adicional: Si con la descripción no tienes suficiente, puedes hacer uso de este campo, que te permite describir con más detalle el contenido el contenido de tu archivo.</li>
+               <li><strong>Descripción:</strong> Consiste en una breve definición del archivo. Procura que sea lo más descriptiva y concreta posible para que el resto de usuarios sepan qué van a encontrarse.</li>
+               <li><strong>Tags de búsqueda:</strong> De forma opcional, puedes introducir tags que faciliten la búsqueda del archivo. Estos tags no saldrán en la información detallada ni en los listados.</li>
+               <li><strong>Tipo de archivo:</strong> Selecciona el tipo de archivo que mejor encaje con el recurso que vas a compartir.</li>
+               <li><strong>Ruta del archivo:</strong> Selecciona la ruta donde se encuentra el archivo a compartir.</li>
+               <li><strong>Información adicional:</strong> Si con la descripción no tienes suficiente, puedes hacer uso de este campo, que te permite describir con más detalle el contenido el contenido de tu archivo.</li>
            </ol>
            <p>Haz click en Enviar, y si tu archivo se ha compartido correctamente visualizarás una ventana verde con un mensaje confirmando el éxito de la operación. En cambio, si ha habido algún problema aparecerá una ventana roja avisando de la incidencia.</p>
            <h1>5  ¿Puedo acceder a los archivos que he compartido?</h1>
            <p>Puedes acceder a ellos y a los archivos de otros usuarios sobre los que te hayan otorgado permisos desde la opción de Mis Archivos.</p>
 	       <p>Aparecerá un listado con información resumida de todos tus archivos en propiedad o con permisos, con distintas opciones sobre cada uno de ellos:</p>
 	       <ol>
-	           <li>Detalle: acceso a información detallada del archivo. Desde esa pantalla puedes descargar tu archivo.</li>
-	           <li>Descargar: puedes descargar tu archivo directamente seleccionando esta opción.</li>
-	           <li>Editar: para modificar la información relativa al archivo o incluso sustituirlo por otro.</li>
-	           <li>Permisos: sólo para tus archivos en propiedad. Otorga permisos de edición sobre tus archivos a otros usuarios.</li>
-	           <li>Eliminar: sólo para tus archivos en propiedad. Elimina el archivo seleccionado si consideras que ya no debe seguir formando parte del catálogo ClickClass.</li>
+	           <li><strong>Detalle:</strong> acceso a información detallada del archivo. Desde esa pantalla puedes descargar tu archivo.</li>
+	           <li><strong>Descargar:</strong> puedes descargar tu archivo directamente seleccionando esta opción.</li>
+	           <li><strong>Editar:</strong> para modificar la información relativa al archivo o incluso sustituirlo por otro.</li>
+	           <li><strong>Permisos:</strong> sólo para tus archivos en propiedad. Otorga permisos de edición sobre tus archivos a otros usuarios.</li>
+	           <li><strong>Eliminar:</strong> sólo para tus archivos en propiedad. Elimina el archivo seleccionado si consideras que ya no debe seguir formando parte del catálogo ClickClass.</li>
 	       </ol>
 	       <h1>6  ¿Cómo funcionan los permisos a otros usuarios?</h1>
 	       <p>La gestión de permisos sobre tus archivos es extremadamente sencilla. Al acceder a Permisos, aparece un listado de los usuarios con un filtrador que te permitirá buscar el/los usuarios que te interesen.</p>
@@ -94,23 +94,23 @@
            <p>Por supuesto que sí, es lo que le da sentido a todo esto. Seleccionando Buscar tienes a tu alcance todos los archivos de nuestro catálogo. Podrás acotar tu búsqueda con la ayuda del filtrador en la esquina superior derecha de la tabla.</p>
            <p>Para cada archivo del listado dispones de dos posibles acciones:</p>
            <ol>
-               <li>Detalle: acceso a información detallada del archivo. Desde esa pantalla puedes descargar el archivo.</li>
-               <li>Descargar: puedes descargar el archivo directamente seleccionando esta opción.</li>
+               <li><strong>Detalle:</strong> acceso a información detallada del archivo. Desde esa pantalla puedes descargar el archivo.</li>
+               <li><strong>Descargar:</strong> puedes descargar el archivo directamente seleccionando esta opción.</li>
            </ol>
            <h1> 8  ¿Tengo el control de la información de mi perfil?</h1>
            <p>Tú eres el administrador de tu información personal. Haciendo click en tu nombre de usuario, en la parte derecha de la cabecera, puedes, además de salir de la aplicación, acceder a tu perfil.</p>
            <p>En esa pantalla podrás ver toda tu información personal, y dispondrás de dos opciones:</p>
            <ol>
-               <li>Editar perfil: accederás a un formulario con tus datos, y podrás modificar los que quieras, salvo el id de usuario. El password también podrás modificarlo desde allí si así lo deseas. Si quieres conservar el mismo simplemente deja ese campo en blanco.</li>
-               <li>Eliminar perfil: puedes darte de baja en el momento en que lo desees. Simplemente ten en cuenta que aquellos archivos que no elimines antes de marcharte permanecerán en el catálogo.</li>
+               <li><strong>Editar perfil:</strong> accederás a un formulario con tus datos, y podrás modificar los que quieras, salvo el id de usuario. El password también podrás modificarlo desde allí si así lo deseas. Si quieres conservar el mismo simplemente deja ese campo en blanco.</li>
+               <li><strong>Eliminar perfil:</strong> puedes darte de baja en el momento en que lo desees. Simplemente ten en cuenta que aquellos archivos que no elimines antes de marcharte permanecerán en el catálogo.</li>
            </ol>
            <h1>9  Y además…</h1>
            <p>En la cabecera de la aplicación tienes acceso a nuestro foro de usuarios y a la sección de ayuda.</p>
-           <p>Si necesitas información adicional, puedes ponerte en contacto con nosotros a través de la dirección de correo clickclass2017@gmail.com.</p>
+           <p>Si necesitas información adicional, puedes ponerte en contacto con nosotros a través de la dirección de correo <strong>clickclass2017@gmail.com</strong>.</p>
            <br>
            <h1>SHARE,</h1>
-           <h1 style="text-indent:20%">SEARCH,</h1>
-           <h1 style="text-indent:40%">ENJOY!</h1>
+           <h1 style="text-indent:12%">SEARCH,</h1>
+           <h1 style="text-indent:26%">ENJOY!</h1>
         </div>
     </body>
 </html>
